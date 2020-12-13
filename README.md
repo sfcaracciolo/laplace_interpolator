6c13051e7a46bf65049268376e3c98e2303b8981
scaracciolo <santiago.caracciolo@gmail.com>
2020-12-13 16:27:48 -0300

    push first version

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # laplace_interpolator
-Laplace interpolator based on "Interpolation on a triangulated 3D surface" paper written by Thom F. Oostendorp et al.
+Laplace interpolator based on "Interpolation on a triangulated 3D surface" paper written by Thom F. Oostendorp et al. The source code compute an interpolation of bad channels based on Laplace operator. The only dependece is Numpy and the implementation use broadcasting rules.
+
+This code is used to ECGI (Electrocardiographic Imaging) research. Possibly some improvements/optimizations can be made using symmetric and sparse matrix structure in cases with geometries has several points but typical cases with 100 to 2000 nodes not requiere this optimizations.
+
+# Usage
+
+Clone the repository in some PATH and import it in your proyect as:
+
+```python
+import sys
+sys.path.insert(0,PATH+'laplace_interpolator/src/') 
+from laplace_interpolator import laplace_interpolator
+
+interpolated = laplace_interpolator((nodes, faces), measured, bad_channels, copy=True)
+```
+Please, if you use this fragment, contact me at scaracciolo@conicet.gov.ar
---
+++ src/laplace_interpolator.py
@@ -0,0 +1,114 @@
+import numpy as np
+
+def neighbour_distance_matrix(nodes, faces):
+    """
+    Retorna la matriz de distancia para los nodos vecinos.
+
+    **Parámetros** Recibe los nodos y caras que conforman el mesh de una geometría.
+
+    nodes: np.ndarray, shape: (N, 3), dtype=np.float32 
+    faces: np.ndarray, shape: (M, 3), dtype=np.int32 
+    
+    **Salida** Retorna una matriz simétrica con valores nulos para los nodos no vecinos y la distancia entre nodos vecinos. Además, la diagonal es nula.
+
+    H: np.ndarray, shape: (N, N), dtype=np.float32 
+
+    """
+    distance = lambda x, y: np.linalg.norm(nodes[x]-nodes[y])
+
+    n = nodes.shape[0]
+    H = np.zeros((n, n), dtype=np.float32)
+    for p1, p2, p3 in faces:
+
+        if H[p1, p2] == 0:
+            H[p1, p2] = distance(p1, p2)
+            H[p2, p1] = H[p1, p2]
+            
+        if H[p1, p3] == 0:
+            H[p1, p3] = distance(p1, p3)
+            H[p3, p1] = H[p1, p3]
+            
+        if H[p2, p3] == 0:
+            H[p2, p3] = distance(p2, p3)
+            H[p3, p2] = H[p2, p3]
+        
+    return H
+
+def laplace_operator(nodes, faces):
+    """
+    Retorna el operador de Laplace para una dada geometría. La construcción del operador se basa en la descripción de (1).
+ 
+    **Parámetros** Recibe los nodos y caras que conforman el mesh de una geometría.
+
+    nodes: np.ndarray, shape: (N, 3), dtype=np.float32 
+    faces: np.ndarray, shape: (M, 3), dtype=np.int32 
+    
+    **Salida** Retorna una matriz no simétrica con valores nulos para los nodos no vecinos y valores no nulos entre nodos vecinos. Además, la diagonal es no nula.
+
+    L: np.ndarray, shape: (N, N), dtype=np.float32 
+    
+    1. Oostendorp TF, van Oosterom A, Huiskamp G. Interpolation on a triangulated 3D surface. Journal of Computational Physics. 1989 Feb 1;80(2):331–43. 
+    """
+
+    H = neighbour_distance_matrix(nodes, faces)
+    
+    # a = np.count_nonzero(H, axis=1) # cantidad de vecinos directos de cada nodo.
+    b = 1/np.sum(H, axis=1) # promedio de distancias de vecinos directos de cada nodo, sin dividir por el total de vecinos e invertido
+    C = np.divide(1, H, out=np.zeros_like(H), where=H!=0) # inversión punto a punto de H, usando 0 en 1/0 = inf.
+    c = np.sum(C, axis=1) # promedio de distancias invertidas de vecinos directos de cada nodo, sin dividir por el total de vecinos.
+
+    L = C*b[:, np.newaxis] # cómputo de lij/4
+    np.fill_diagonal(L, -c*b) # cómputo de lii/4
+    L *= 4
+    
+    return L
+
+def laplace_interpolator(mesh, measured, bad_channels, copy=False):
+    """
+    Retorna las mediciones interpoladas sobre la geometría utilizando el método B descripto en (1).
+
+    **Parámetros**
+
+    mesh: tuple (nodes, faces)
+        nodes: np.ndarray, shape: (N, 3), dtype=np.float32 
+        faces: np.ndarray, shape: (M, 3), dtype=np.int32 
+    
+    measured:  np.ndarray, shape: (N, T), dtype=np.float32. Son las mediciones de cada nodo a lo largo del tiempo.
+
+    bad_channels: np.ndarray, shape: (P, ), dtype=np.int32. Son los índices de los nodos a interpolar.
+
+    copy: bool. Si es True, retorna una nueva matriz, si es False, modifica "measured" en las files de los canales mal medidos.
+
+    **Salida** Retorna una matriz no simétrica con valores nulos para los nodos no vecinos y valores no nulos entre nodos vecinos. Además, la diagonal es no nula.
+
+    interpolated: np.ndarray, shape: (N, T), dtype=np.float32. Son las mediciones de la variable "measured" con las señales de los canales malos interpoladas.
+
+    1. Oostendorp TF, van Oosterom A, Huiskamp G. Interpolation on a triangulated 3D surface. Journal of Computational Physics. 1989 Feb 1;80(2):331–43. 
+    """
+
+    nodes, faces = mesh
+    L = laplace_operator(nodes, faces)
+
+    channels = np.arange(L.shape[0], dtype=np.int32)
+    good_channels = np.delete(channels, bad_channels)
+
+    L11 = L[bad_channels[:, np.newaxis], bad_channels]
+    L12 = L[bad_channels[:, np.newaxis], good_channels]
+    L21 = L[good_channels[:, np.newaxis], bad_channels]
+    L22 = L[good_channels[:, np.newaxis], good_channels]
+
+    f2 = np.delete(measured, bad_channels, axis=0)
+
+    Y = -np.vstack((L12, L22))@f2
+    A = np.vstack((L11, L21))
+
+    f1 = np.linalg.inv(A.T@A)@A.T@Y
+    
+    if copy:
+        interpolated = measured.copy()
+        interpolated[bad_channels] = f1
+    else:
+        measured[bad_channels] = f1
+        interpolated = measured
+
+    return interpolated
